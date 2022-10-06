d4e6d64f902ed1f2d3d6a70d2d0020baa5bd87b3
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-10-06 07:52:55 +0530

    Create Bubble Sort.java

---
+++ DSA A-Z Repo/Sorting/Bubble Sort.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+    ///JAI SHRI RAM 
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
