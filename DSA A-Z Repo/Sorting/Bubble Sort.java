c227f636228a9b098037f23d8596e112ead337af
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-10-06 08:05:47 +0530

    Update Bubble Sort.java

--- DSA A-Z Repo/Sorting/Bubble Sort.java
+++ DSA A-Z Repo/Sorting/Bubble Sort.java
@@ -6,13 +6,17 @@ class Solution
     {
         //code here
         for(int i = 0; i < n-1; i++){
-            for(int j = 0; j < n-1; j++){
+            boolean swapped = false;
+            for(int j = 0; j < n-i-1; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
             }
+            if(!swapped)
+                break;
         }
     }
 }
