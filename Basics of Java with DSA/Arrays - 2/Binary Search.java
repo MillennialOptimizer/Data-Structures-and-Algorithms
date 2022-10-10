1d38ac1391a65b9537c00a3829a038f459748028
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-10-10 18:51:50 +0530

    Update Binary Search.java

--- Basics of Java with DSA/Arrays - 2/Binary Search.java
+++ Basics of Java with DSA/Arrays - 2/Binary Search.java
@@ -1,3 +1,19 @@
+// Linear Search 
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int ind = -1;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == k){
+                ind = i;
+                break;
+            }
+        }
+        return ind;
+    }
+}
+
+// Binary Search 
 
 public class Solution { 
 
