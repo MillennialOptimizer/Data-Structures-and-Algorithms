cb09b44bb969092396ce41bcf8230a8efa227946
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-10-12 23:39:52 +0530

    Update Rotate Array.java

--- Basics of Java with DSA/Arrays - 2/Rotate Array.java
+++ Basics of Java with DSA/Arrays - 2/Rotate Array.java
@@ -1,21 +1,27 @@
-
-public class Solution {  
-
-    public static void rotate(int[] arr, int d) {
-    	//Your code goes here
-        int n = arr.length;
-        int[] rArr = new int[n];
-        for(int i = 0; i < n; i++){
-            if(i < d){
-                rArr[n-d+i] = arr[i];
-            }
-            else{
-                rArr[i-d] = arr[i];
-            }
+class Solution
+{
+    static void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
-        for(int i = 0; i < n; i++){
-            arr[i] = rArr[i];
+        System.out.println("");
+    }
+    //Function to rotate an array by d elements in counter-clockwise direction.
+    static void reverse(int[] arr, int l, int r){
+        while(r >= l){
+            int temp = arr[r];
+            arr[r] = arr[l];
+            arr[l] = temp;
+            r--;
+            l++;
         }
     }
-
+    static void rotateArr(int arr[], int d, int n)
+    {
+        // add your code here
+        d = d%n;
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, n-d-1);
+        reverse(arr, n-d, n-1);
+    }
 }
