7b65a5792610d59ae6204418ff4e90748204ba8d
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-10-12 22:57:40 +0530

    Update Sort 0 1.java

--- Basics of Java with DSA/Arrays - 1/Sort 0 1.java
+++ Basics of Java with DSA/Arrays - 1/Sort 0 1.java
@@ -18,3 +18,23 @@ public class Solution {
         
     }
 }
+
+// space optimized
+public class Solution {  
+
+    public static void sortZeroesAndOne(int[] arr) {
+    	//Your code goes here
+        int l = 0, r = 0; 
+        int n = arr.length; 
+        while(l < n){
+            while(r < n && arr[r] == 0)
+                r++;
+            if(l > r && arr[l] == 0){
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+            l++;
+        }
+    }
+}
