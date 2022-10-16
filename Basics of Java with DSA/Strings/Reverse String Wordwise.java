00f165415603239f30bab742cbfdd6f29e2ea9cd
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-10-16 17:34:19 +0530

    Update Reverse String Wordwise.java

--- Basics of Java with DSA/Strings/Reverse String Wordwise.java
+++ Basics of Java with DSA/Strings/Reverse String Wordwise.java
@@ -1,37 +1,28 @@
-
 public class Solution {
-	public static String reverse(String s){
-        String res = "";
-        for(int i = s.length()-1; i >= 0; i--){
-            res += s.charAt(i) + "";
-        }
-        return res;
-    }
-	public static String reverseWordWise(String str) {
-		//Your code goes here
-        int n = str.length(); 
-        StringBuilder s = new StringBuilder(str);
-        s.reverse();
-        StringBuilder word = new StringBuilder();
-        String res = "";
-        for(int i = 0; i < n; i++){
-            if(s.charAt(i) == ' '){
-                String reversedWord = reverse(word.toString());
-                res += reversedWord; 
-                res += " ";
-                word = new StringBuilder();
+	public static String reverseWordWise(String s) {
+		// Write your code here
+        StringBuffer str = new StringBuffer();
+        int n = s.length(); 
+        StringBuffer word = new StringBuffer();
+        for(int i = n-1; i >= 0; i--){
+            char ch = s.charAt(i);
+            if(i == 0){
+                word.append(ch);
+                word.reverse();
+                str.append(word);
+                word = new StringBuffer();
             }
-            else if(i == n-1){
-                word.append(s.charAt(i));
-                String reversedWord = reverse(word.toString());
-                res += reversedWord; 
-                word = new StringBuilder();
+            else if(s.charAt(i) == ' '){
+                word.reverse();
+                str.append(word);
+                str.append(' ');
+                word = new StringBuffer();
             }
             else{
-                word.append(s.charAt(i));
+                word.append(ch);
             }
         }
-        return res;
-	}
+        return str.toString();
 
+	}
 }
