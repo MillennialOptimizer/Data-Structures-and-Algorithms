144f06ad615755a4f7c0df0eeca152a6fb5679d2
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-12-20 21:57:45 +0530

    Update Recursive Multiplication.java

--- Basics of Java with DSA/Recursion Assignement/Recursive Multiplication.java
+++ Basics of Java with DSA/Recursion Assignement/Recursive Multiplication.java
@@ -3,6 +3,8 @@ public class solution {
 
 	public static int multiplyTwoIntegers(int m, int n){
 		// Write your code here
+		if(m == 0 || n == 0)
+			return 0;
 		if(n == 1)
 			return m; 
 		return multiplyTwoIntegers(m, n-1) + m;
