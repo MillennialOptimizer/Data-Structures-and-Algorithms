81277505a53328a40ca1f925bff1a266d4fb1f3e
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2022-12-22 22:24:10 +0530

    Update Intersection Of Two Arrays.java

--- Basics of Java with DSA/Arrays - 1/Intersection Of Two Arrays.java
+++ Basics of Java with DSA/Arrays - 1/Intersection Of Two Arrays.java
@@ -2,17 +2,20 @@
 public class Solution{  
     
     public static void intersections(int arr1[], int arr2[]) {
-    	//Your code goes here
-        int n = arr1.length; 
-        int m = arr2.length;
-        int[] flag = new int[m];
+
+        int n = arr1.length, m = arr2.length; 
+        boolean isCovered[] = new boolean[m]; // checks if the element in arr2 is already in an intersection or not 
         for(int i = 0; i < n; i++){
+            boolean isExisting = false;
             for(int j = 0; j < m; j++){
-                if(arr1[i] == arr2[j] && flag[j] == 0){
-                    System.out.print(arr1[i] + " ");
-                    flag[j] = 1;
+                if(arr2[j] == arr1[i] && isCovered[j] == false){
+                    isExisting = true;
+                    isCovered[j] = true;
                     break;
                 }
+            }
+            if(isExisting){
+                System.out.print(arr1[i] + " ");
             }
         }
     }
