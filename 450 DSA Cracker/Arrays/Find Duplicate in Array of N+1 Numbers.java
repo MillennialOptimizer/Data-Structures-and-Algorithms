f23e6300a10c1fbb1195f2cb3bdd5f65bceff0bd
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2023-01-11 22:54:42 +0530

    Update Find Duplicate in Array of N+1 Numbers.java

--- 450 DSA Cracker/Arrays/Find Duplicate in Array of N+1 Numbers.java
+++ 450 DSA Cracker/Arrays/Find Duplicate in Array of N+1 Numbers.java
@@ -11,3 +11,24 @@ class Solution {
         return -1;
     }
 }
+
+
+// most optimized 
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // JAI SHRI RAM 
+        int n = nums.length; 
+        int slow = nums[0], fast = nums[0]; 
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast); // we do a do while loop as it should break when slow equals fast 
+
+        fast = nums[0]; 
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
